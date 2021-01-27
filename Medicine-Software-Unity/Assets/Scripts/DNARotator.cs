c5a7e30adf8cbe08cf281b2cc899532c9a1f5cd4
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2021-01-27 18:13:56 +0200

    Implement better UI

---
+++ Medicine-Software-Unity/Assets/Scripts/DNARotator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNARotator : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(speed, 0, 0));
+    }
+}
