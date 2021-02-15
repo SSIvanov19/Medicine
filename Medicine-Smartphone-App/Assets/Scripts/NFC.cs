e0b7677e84b59a745d9c1d38a254998082f0654e
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2021-02-15 18:33:37 +0200

    Fix merge conflict

--- Medicine-Smartphone-App/Assets/Scripts/NFC.cs
+++ Medicine-Smartphone-App/Assets/Scripts/NFC.cs
@@ -62,11 +62,7 @@ public class NFC : MonoBehaviour
                     }
                     else if (sAction == "android.nfc.action.TAG_DISCOVERED")
                     {
-<<<<<<< HEAD
                         debugOutputText.text = "This type of tag is not supported !";
-=======
-                        Debug.Log("This type of tag is not supported !");
->>>>>>> 08f1b718a4c812eb39e413d4cb932e21133f26f6
                     }
                     else
                     {
@@ -76,19 +72,9 @@ public class NFC : MonoBehaviour
                 catch (Exception ex)
                 {
                     var text = ex.Message;
-<<<<<<< HEAD
                     debugOutputText.text = "Catch";
                 }
             }
-=======
-                    tagOutputText.text = text;
-                }
-            }
-            else
-            {
-                debugOutputText.text = "No NFC found";
-            }
->>>>>>> 08f1b718a4c812eb39e413d4cb932e21133f26f6
         }
         else
         {
